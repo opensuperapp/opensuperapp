5fde2f46457dbb5b31c078f4cfe43bb9ca4c0c4c
v15a1 <visalrajapakse@gmail.com>
2026-01-07 10:02:06 +0530

    Update licence headers

--- frontend/app/(tabs)/apps/notifications.tsx
+++ frontend/app/(tabs)/apps/notifications.tsx
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 WSO2 LLC. (https://www.wso2.com).
+// Copyright (c) 2026 WSO2 LLC. (https://www.wso2.com).
 //
 // WSO2 LLC. licenses this file to you under the Apache License,
 // Version 2.0 (the "License"); you may not use this file except
@@ -73,7 +73,7 @@ export default function Notifications() {
   );
 
   const renderItem = ({ item }: any) => {
-    const itemTime = convertToUtc(item.createdAt).getTime();
+    const itemTime = convertToUtc(item.createdAt)?.getTime() ?? 0;
     return (
       <NotificationItem
         item={item}
