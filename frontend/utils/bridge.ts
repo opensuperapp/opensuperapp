0f50f9b4370bdc9144a73da6fd24e77bc0a21133
v15a1 <visalrajapakse@gmail.com>
2026-01-26 12:08:12 +0530

    Add bridge functionality to open existing microapp

--- frontend/utils/bridge.ts
+++ frontend/utils/bridge.ts
@@ -41,6 +41,8 @@ export const TOPIC = {
   DEVICE_SCREEN_SIZE: "device_screen_size",
   OPEN_URL: "open_url",
   MICRO_APP_VERSION: "micro_app_version",
+  OPEN_MICRO_APP: "open_micro_app",
+  GET_LAUNCH_DATA: "get_launch_data",
 };
 
 // JavaScript code injected into the WebView to enable communication between
@@ -118,5 +120,8 @@ export const injectedJavaScript = `window.nativebridge = {
     rejectOpenUrl: (err) => console.error("Failed to open URL:", err),
     requestMicroAppVersion: () => window.ReactNativeWebView.postMessage(JSON.stringify({ topic: "micro_app_version" })),
     resolveMicroAppVersion: (version) => console.log("Micro App Version:", version),
-    rejectMicroAppVersion: (err) => console.error("Failed to get Micro App version:", err)
+    rejectMicroAppVersion: (err) => console.error("Failed to get Micro App version:", err),
+    requestOpenMicroApp: (appId, data) => window.ReactNativeWebView.postMessage(JSON.stringify({ topic: "open_micro_app", data: { appId, data } })),
+    requestGetLaunchData: () => window.ReactNativeWebView.postMessage(JSON.stringify({ topic: "get_launch_data" })),
+    resolveGetLaunchData: (data) => console.log("Launch Data:", data)
   };`;
